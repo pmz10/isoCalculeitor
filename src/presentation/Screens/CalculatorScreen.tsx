f05ecbee569299bb51ef4f91fc8fc9118dd06405
pmz10 <welcome_to_pmz@hotmail.com>
2025-03-18 23:41:09 -0600

    Add Pressable button to CalculatorScreen

--- src/presentation/Screens/CalculatorScreen.tsx
+++ src/presentation/Screens/CalculatorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { styles } from '../../Config/Theme/app-theme';
 
 const CalculatorScreen = () => {
@@ -8,6 +8,13 @@ const CalculatorScreen = () => {
             <View style={{paddingHorizontal: 30, marginBottom: 10}}> 
                 <Text style={styles.mainResult}>1500</Text>
                 <Text style={styles.subResult}>15</Text>
+            </View>
+
+            <View style={ styles.row}>
+            <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>1</Text>
+            </Pressable>
+            
             </View>
         </View>
     );
